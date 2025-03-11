ad728be3c1778d5916f688ef54c042315df39cfe
quocta-uranus <taquoc196@gmail.com>
2025-03-12 01:29:31 +0700

    init code

---
+++ VNpay/src/java/model/Constant.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package java.model;
+
+/**
+ *
+ * @author Asus
+ */
+public class Constant {
+
+	public static String GOOGLE_CLIENT_ID = "";
+
+	public static String GOOGLE_CLIENT_SECRET = "";
+
+	public static String GOOGLE_REDIRECT_URI = "";
+
+	public static String GOOGLE_LINK_GET_TOKEN = "";
+
+	public static String GOOGLE_LINK_GET_USER_INFO = "";
+
+	public static String GOOGLE_GRANT_TYPE = "";
+
+}
